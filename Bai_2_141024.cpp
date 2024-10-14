bbc41de13f058740bbdd498b0ca1d5e938e23bd8
dothanhlonghust <Long.DT233504@sis.hust.edu.vn>
2024-10-14 22:43:47 +0700

    Add files via upload

--- Bai_2_141024.cpp
+++ Bai_2_141024.cpp
@@ -6,25 +6,21 @@ int main()
     int n;
     cin >> n;
     int arr[n+1]; 
-    // T? 0 d?n n
-    // Ph?n t? m?ng arr này b?ng 1 n?u là SNT 
-    
+
     for(int i=0; i<n+1; i++){
-        arr[i] = 1; // M?i d?u coi toàn b? là SNT
+        arr[i] = 1;
     }
-    arr[0] = arr[1] = 0; // 0 và 1 khác SNT
+    arr[0] = arr[1] = 0; 
     
     for(int i=0; i<n+1; i++){
         if(arr[i]==1){
             
-            // VD: 3 là SNT thì b?i c?a 3 (b?t d?u t? 3*2) khác SNT
             for(int j=2; j < n/i + 1; j++){
                 arr[i*j]=0; 
             }
         }
     }
     
-    // In ra SNT
     for(int i=0; i<n+1; i++){
         if(arr[i]==1){
             cout << i << " ";
@@ -32,8 +28,7 @@ int main()
     }
     cout << "\n";
     
-    // Yêu c?u 1: T?ng 2 SNT là 1 SNT thì trong 2 SNT này ph?i có 2
-    cout << "Các c?p SNT th?a mãn yêu c?u 1: ";
+    cout << "Cac cap SNT thoa man yeu cau 1: ";
     for(int i=2; i<n+1; i++){
         if (arr[i]==1){
             if(arr[i+2]==1){
@@ -42,10 +37,9 @@ int main()
         }
     }
     cout << "\n";
-    
-    // Yêu c?u 2: check SNT
+
     int x;
-    cout << "Nh?p s? c?n tìm: ";
+    cout << "Nhap so can tim: ";
     cin >> x;
     if(arr[x]==1){
         cout << x << " là SNT";
@@ -53,8 +47,6 @@ int main()
         cout << x << " khác SNT";
     }
     
-
     return 0;
 }
-Vi?t cho Tr?n Minh Khoa
 
